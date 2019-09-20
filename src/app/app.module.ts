06c43a33c9f2a12122454a8753a3dc75c573858e
FajsiEx <fajsiexofficial@gmail.com>
2019-09-20 18:21:55 +0200

    + basic site structure & + basic global styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './shell/navbar/navbar.component';
+import { FooterComponent } from './shell/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
